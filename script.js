64ade4b5c2da37faf87b1fce4440fb0f8084eed8
RalphFred <raphaelgbaorun@gmail.com>
2024-03-22 23:27:46 +0100

    build: added animations to the porfolio section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.body.style.overflow = 'hidden'
+// document.body.style.overflow = 'hidden'
 
 function startLoader() {
   let counterElement = document.querySelector('.counter');
@@ -37,11 +37,11 @@ function startLoader() {
   updateCounter();
 }
 
-startLoader();
+// startLoader();
 
 gsap.from(".about--img", 2, {
-  scrollTrigger: ".about--img",
-  x: -600,
+  scrollTrigger: ".about--header",
+  x: "-100%",
   rotation: -45,
   opacity: 0,
   ease: "power4.out"
@@ -49,13 +49,13 @@ gsap.from(".about--img", 2, {
 
 gsap.from(".about--header", 1.5, {
   scrollTrigger: ".about--header",
-  x: 600,
+  x: "100%",
   ease: "power4.out"
 })
 
 gsap.from(".about--text", 1.5, {
   scrollTrigger: ".about--header",
-  x: 600,
+  x: "100%",
   delay: .5,
   ease: "power4.out",
   opacity: 0,
@@ -63,7 +63,7 @@ gsap.from(".about--text", 1.5, {
 
 gsap.from(".about--button", 1.5, {
   scrollTrigger: ".about--header",
-  y: 400,
+  y: "100%",
   delay: 1,
   ease: "power4.out",
   opacity: 0,
@@ -72,7 +72,7 @@ gsap.from(".about--button", 1.5, {
 
 gsap.from(".services--header", 1.5, {
   scrollTrigger: ".services--header",
-  x: -600,
+  x: "-100%",
   ease: "power4.out",
   delay: 1,
   opacity: 0,
@@ -92,6 +92,34 @@ gsap.from(".card", 1, {
   stagger: 0.25,
   ease: "power4.out",
   scrollTrigger: ".card",
+})
+
+gsap.from(".img1", 1, {
+  scrollTrigger: ".img1",
+  y: "100%",
+  opacity: 0,
+  ease: "power4.out",
+})
+
+gsap.from(".img2", 1, {
+  scrollTrigger: ".img2",
+  x: "100%",
+  opacity: 0,
+  ease: "power4.out",
+})
+
+gsap.from(".img3", 1, {
+  scrollTrigger: ".img3",
+  y: "100%",
+  opacity: 0,
+  ease: "power4.out",
+})
+
+gsap.from(".img4", 1, {
+  scrollTrigger: ".img4",
+  x: "100%",
+  opacity: 0,
+  ease: "power4.out",
 })
 
 
